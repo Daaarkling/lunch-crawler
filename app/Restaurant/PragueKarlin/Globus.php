a9e9f82df12ca7c5d904fa9b138f3c91a7d9f1e8
Jan Vaňura <jano.vano@gmail.com>
2018-05-28 22:23:18 +0200

    Globus refactored to Zomato

--- app/Restaurant/PragueKarlin/Globus.php
+++ app/Restaurant/PragueKarlin/Globus.php
@@ -2,67 +2,26 @@
 
 namespace LunchCrawler\Restaurant\PragueKarlin;
 
-use Atrox\Matcher;
-use LunchCrawler\Restaurant\HtmlParseRestaurantLoader;
-use LunchCrawler\Restaurant\Menu\Dish;
-use LunchCrawler\Restaurant\Menu\Menu;
 use LunchCrawler\Restaurant\Restaurant;
-use LunchCrawler\Restaurant\RestaurantEmptyMenuException;
-use LunchCrawler\Restaurant\RestaurantLoadException;
-use Throwable;
+use LunchCrawler\Restaurant\ZomatoRestaurantLoader;
 
-final class Globus extends HtmlParseRestaurantLoader
+final class Globus extends ZomatoRestaurantLoader
 {
 
-	private const MENU_URL = 'http://restauraceglobus.cz/poledni-menu/';
-	private const NAME = 'Restaurace Globus';
+	public function getRestaurantId(): int
+	{
+		return 18257507;
+	}
+
+	public function getName(): string
+	{
+		return 'Restaurace Globus';
+	}
 
 	public function loadRestaurant(): Restaurant
 	{
-		try {
-			$response = $this->httpClient->request('GET', self::MENU_URL);
-			$html = $response->getBody()->getContents();
-
-			$matcherPrices = Matcher::single('//div[@id="main-content"]', [
-				'priceSoup' => './/h4[1]/strong',
-				'priceDish' => './/h4[2]/strong',
-			])->fromHtml();
-
-			/** @var string[] $rawDishes */
-			$prices = $matcherPrices($html);
-			$priceSoup = (int) filter_var($prices['priceSoup'], FILTER_SANITIZE_NUMBER_INT);
-			$priceMeal = (int) filter_var($prices['priceDish'], FILTER_SANITIZE_NUMBER_INT);
-
-			$matcherDishes = Matcher::single(sprintf('//div[@id="main-content"]//div[contains(@class, "et_pb_text_%s")]', date('N')), [
-				'soup' => './/p',
-				'meal' => Matcher::multi('.//ol/li'),
-			])->fromHtml();
-
-			/** @var array $rawDishes */
-			$rawDishes = $matcherDishes($html);
-			$soap = new Dish($rawDishes['soup'], $priceSoup);
-
-			$meals = [];
-			foreach ($rawDishes['meal'] as $meal) {
-				$meals[] = new Dish($meal, $priceMeal);
-			}
-
-			// last one is dessert
-			array_pop($meals);
-
-			$menu = Menu::createFromDishes([$soap], $meals);
-
-			if ($menu->isEmpty()) {
-				throw new RestaurantEmptyMenuException(self::NAME);
-			}
-
-			return new Restaurant(self::NAME, $menu);
-
-		} catch (RestaurantEmptyMenuException $e) {
-			throw $e;
-		} catch (Throwable $e) {
-			throw new RestaurantLoadException(self::NAME, $e);
-		}
+		self::$soapLimitPrice = 36;
+		return parent::loadRestaurant();
 	}
 
 }
--- www/index.php
+++ www/index.php
@@ -5,14 +5,14 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 
 /** @var \Nette\DI\Container $container */
-$container = require __DIR__ . '/../bootstrap.php';
+$container = require __DIR__ . '/../app/bootstrap.php';
 
 /** @var \LunchCrawler\Command\RunCommand $runCommand */
 $runCommand = $container->getByType(RunCommand::class);
 
 $output = new NullOutput();
 $input = new ArrayInput([
-
+	'--output' => 'slack'
 ]);
 
 $statusCode = $runCommand->run($input, $output);

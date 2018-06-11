340f93089b407f1d83441bf5bd0f3c1e9f92051b
Jan Vaňura <jano.vano@gmail.com>
2018-06-11 21:35:28 +0200

    Token protection

--- www/index.php
+++ www/index.php
@@ -1,11 +1,27 @@
 <?php declare(strict_types = 1);
 
 use LunchCrawler\Command\RunCommand;
+use Nette\Http\Request;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 
 /** @var \Nette\DI\Container $container */
 $container = require __DIR__ . '/../app/bootstrap.php';
+
+$params = $container->getParameters();
+
+if (!array_key_exists('wwwSecret', $params)) {
+	echo 'Define `wwwSecret` in neon parameters section.';
+	exit(1);
+}
+
+/** @var \Nette\Http\Request $request */
+$request = $container->getByType(Request::class);
+
+if ($request->getQuery('secret') !== $params['wwwSecret']) {
+	echo 'Secret token is not valid.';
+	exit(1);
+}
 
 /** @var \LunchCrawler\Command\RunCommand $runCommand */
 $runCommand = $container->getByType(RunCommand::class);

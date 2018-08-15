694fbc29d60877198612dc9af2a8003e2ac59696
Jan Vaňura <jano.vano@gmail.com>
2018-08-15 22:42:55 +0200

    Podolka added

---
+++ app/Restaurant/PragueKarlin/Podolka.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace LunchCrawler\Restaurant\PragueKarlin;
+
+use Atrox\Matcher;
+use LunchCrawler\Restaurant\HtmlParseRestaurantLoader;
+use LunchCrawler\Restaurant\Menu\Menu;
+use LunchCrawler\Restaurant\Restaurant;
+use LunchCrawler\Restaurant\RestaurantEmptyMenuException;
+use LunchCrawler\Restaurant\RestaurantLoadException;
+use Throwable;
+
+final class Podolka extends HtmlParseRestaurantLoader
+{
+
+	private const MENU_URL = 'http://karlin.restauracepodolka.cz/';
+	private const NAME = 'Restaurace Podolka';
+
+	public function loadRestaurant(): Restaurant
+	{
+		try {
+			$response = $this->httpClient->request('GET', self::MENU_URL);
+			$html = $response->getBody()->getContents();
+
+			$matcher = Matcher::single('//img[@class="fl-photo-img"]/@src')->fromHtml();
+
+			/** @var string $imageSrc */
+			$imageSrc = $matcher($html);
+
+			$menu = Menu::createFromImageUrl($imageSrc);
+
+			if ($menu->isEmpty()) {
+				throw new RestaurantEmptyMenuException(self::NAME);
+			}
+
+			return new Restaurant(self::NAME, $menu);
+
+		} catch (RestaurantEmptyMenuException $e) {
+			throw $e;
+		} catch (Throwable $e) {
+			throw new RestaurantLoadException(self::NAME, $e);
+		}
+	}
+
+}
